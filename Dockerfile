4e12483228639f6d7c58145c972fa1059e42131a
Ivan Kuznetsov <kuzma.wm@gmail.com>
2018-05-09 18:39:03 +0300

    Remove stale files related to old Docker workflow

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,7 @@ COPY ./public /app/public
 COPY ./src /app/src
 COPY ./package.json /app/
 COPY ./yarn.lock /app/
-COPY ./run.sh /root/
 
 RUN yarn install
-
 CMD yarn start
 
--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-set -x
-
-PATH_JS=/usr/share/nginx/html/static/js/main.*
-
-sed -i "s#__REACT_APP_OAUTH_URL_PLACEHOLDER__#${POSTGREST_OAUTH_API_URI}#g" $PATH_JS
-
-nginx -g 'daemon off;'
