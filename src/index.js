3026a0ab6158934344497256064e38ee9166b369
kuisis <kuysis@gmail.com>
2018-05-29 16:03:32 +0300

    new styles for easier customizing

--- src/index.js
+++ src/index.js
@@ -3,13 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createMuiTheme } from 'material-ui/styles';
 
-fetch(`${process.env.PUBLIC_URL}/language.json`, { method: 'GET' })
+fetch(`${process.env.PUBLIC_URL}/settings.json`, { method: 'GET' })
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      ReactDOM.render(<App language = {data.language} />, document.getElementById('root'));
+      let theme = createMuiTheme({
+        palette: {
+          primary: { main: data.styles.activeTabColor}
+        },
+        overrides: {
+          MuiTabs: { indicator: {backgroundColor: data.styles.tabIndicator} },
+          MuiTab: { textColorPrimary: { color: data.styles.tabTextColor } },
+          MuiInputLabel: { root: {color: data.styles.inputLabelColor} },
+          MuiInput: { 
+            root: {color: data.styles.inputTextColor},
+            underline: {'&:after': {backgroundColor: data.styles.underlineActiveColor}},
+            error: {'&:after': {backgroundColor: data.styles.underlineErrorColor}} 
+          },
+          MuiFormHelperText: { error: {color: data.styles.errorTextColor} },
+          MuiButton: { 
+            raisedPrimary: { 
+              backgroundColor: data.styles.buttonColor, 
+              color: data.styles.buttonTextColor, 
+              '&:hover': { color: data.styles.buttonTextHoverColor, backgroundColor: data.styles.buttonHoverColor }
+            }, 
+            disabled: {color: data.styles.disabledButtonTextColor} 
+          },
+          MuiTypography: { colorPrimary: {color: data.styles.regularTextColor} }
+        }
+      });
+      ReactDOM.render(<App language = {data.language} theme = {theme} />, document.getElementById('root'));
     })
 
 registerServiceWorker();
